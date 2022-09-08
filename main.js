ea1d42640972bfd89ee117fb114e249c902ad16e
taokyone <antonsson.andreas@hotmail.com>
2022-09-08 17:45:32 +0200

    session storage done. Part 1

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 
 //Eventlisteners for "mousing over" the the mobile buttons!
 
-document.getElementById("session-storage").addEventListener("mouseover", displayStorageIn);
-document.getElementById("session-storage").addEventListener("mouseout", displayStorageOut);
+document.getElementById("btn-mobile").addEventListener("mouseover", displayStorageIn);
+document.getElementById("btn-mobile").addEventListener("mouseout", displayStorageOut);
 
-//Functions that activates when the mobile buttons are "moused over".
+//Functions that activates "Submit notes" when the mobile buttons are "moused over".
 
 function displayStorageIn() {
   document.getElementById("message-to-user").innerHTML = "Submit notes!";
@@ -13,4 +13,50 @@ function displayStorageOut() {
   document.getElementById("message-to-user").innerHTML = "";
 }
 
+
 /* Session Storage */
+/* 
+problem
+  get data and put it into the "aside-item" element
+
+solution
+-save function works
+
+
+*/
+
+function doFirst () {
+  var button = document.getElementById("btn-mobile");
+  button.addEventListener("click", save);
+
+}
+
+ function save() {
+ 
+   let saveTitle = document.getElementById("title").value;
+
+   let saveDueDate = document.getElementById("due-date").value;
+   sessionStorage.setItem(saveTitle, saveDueDate); 
+   
+  displayData();
+
+  document.getElementById("title").value = "";
+  document.getElementById("due-date").value = "";
+
+ }
+
+ function displayData() {
+  var asideContent = document.getElementById("aside-items");
+  asideContent.innerHTML = "";
+
+     for (var i = 0; i < sessionStorage.length; i++) {
+     var a = sessionStorage.key(i);
+     var b = sessionStorage.getItem(a);
+    asideContent.innerHTML += a + " - " + b + "<br />";
+    /* skriver in värdena i rutan till höger under "summa". */
+    }
+
+ }
+
+
+ //window.addEventListener("load", doFirst);
